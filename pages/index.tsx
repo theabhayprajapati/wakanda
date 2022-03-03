7bdf51257f33b9bfc0272988f9004bedf9c00861
theabhayprajapati <theabhayprajapati@gmail.com>
2022-03-03 20:22:46 +0530

    added translation feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,55 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-
+import { useEffect, useRef, useState } from 'react'
+import ReactTextareaAutosize from 'react-textarea-autosize'
 const Home: NextPage = () => {
+  const [userInput, setUserInput] = useState()
+  const textInput: any = useRef(null);
+  useEffect(() => {
+    textInput.current.focus();
+  }, []);
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Wakanda Forever</title>
+        {/* add text in favicon */}
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main className='max-w-7xl mx-auto  p-5  '>
+        <header className='text-gray-50 flex flex-col h-full justify-center items-center gap-3 font-OpenSans font-semibold text-2xl'>
+          <h1>
+            English to Wakandian
+          </h1>
+          {/* <hr className='self-center w-[30%] border-blue-800 border-2 rounded-lg' /> */}
+        </header>
+        <div className='mt-5 md:flex'>
+          <section>
+            <h1 className='text-gray-50 text-xl'>
+              English
+            </h1>
+            <ReactTextareaAutosize ref={textInput} value={userInput} onChange={(e: any) => setUserInput(e.target.value)} placeholder='Enter Text' className='text-4xl border-none outline-none text-white placeholder:text-white font-medium cursor-text' />
+          </section>
+          <hr className='max-w-[30%] md:hidden mx-auto border-blue-800 border-2 rounded-lg' />
+          <h1 className='hidden md:inline-block text-blue-800'>
+            |
+          </h1>
+          <section>
+            <h1 className='text-blue-300 text-xl'>
+              Wakandian
+            </h1>
+            <ReactTextareaAutosize value={userInput} className="outline-none font-Wakandian text-4xl text-white">
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+            </ReactTextareaAutosize>
+          </section>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
+
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+
     </div>
   )
 }
